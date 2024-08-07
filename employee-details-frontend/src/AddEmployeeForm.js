ed4b2984ef5e6ed891f25fbb1ca2bf876711214d
msoncsswe <miris11260@gmail.com>
2024-08-07 14:58:20 -0400

    add date validation, event handler to update changes, and state to store input fields

--- employee-details-frontend/src/AddEmployeeForm.js
+++ employee-details-frontend/src/AddEmployeeForm.js
@@ -1,23 +1,58 @@
 import './AddEmployeeForm.css'
 
+import { useState } from 'react';
+
 export default function AddEmployeeForm({showForm, setShowForm}) {
-  const employeeInfo = ['Last Name', 'First Name', 'DOB', 'email', 'Country'];
+
+  const [employeeInfo, setEmployeeInfo] = useState({
+    'lastName': '',
+    'firstName': '',
+    'DOB': '',
+    'email': '',
+    'country': ''
+  })
+
   const infoFields = [];
 
-  for (const detail of employeeInfo) {
-    const attributeName = detail.toLowerCase().replace(' ', '-');
+  const handleChange = e => {
+    let {name, value} = e.target;
+    e.preventDefault();
+
+    setEmployeeInfo((prevEmployeeInfo) => ({
+      ...prevEmployeeInfo, [name]: value
+    }))
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // fetch request to add employeeInfo to DB here. 
+
+    console.log('Add new employee:', employeeInfo)
+
+  }
+
+  for (const detail in employeeInfo) {
+
+    const inputLame = (() => {
+      if(detail === 'lastName') return 'Last Name';
+      if(detail === 'firstName') return 'First Name';
+      else return detail[0].toUpperCase() + detail.slice(1);
+    })();
+
+    const inputType = detail === 'DOB' ? 'date' : 'text';
 
     infoFields.push(
       <>
-        <label htmlFor={attributeName}>{detail}</label>{' '}
-        <input className='add-employee-input' type='text' id={attributeName} required />
+        <label htmlFor={detail}>{inputLame}</label>
+        <input name={detail} type={inputType} value={employeeInfo[detail]} onChange={handleChange} required />
       </>
     )
   };
 
   return (showForm) ? (
     <div className="popup">
-    <form id='add-employee-form' action='submit'>
+    <form id='add-employee-form' action='submit' onSubmit={handleSubmit}>
       {infoFields}
       <div id='add-employee-buttons'>
         <button id="add">Submit</button>
