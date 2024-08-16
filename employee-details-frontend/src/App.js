d3900b78df656fc8d6790ed548b4353edd0ca8b1
Will <=>
2024-08-16 12:52:17 -0700

    added tailwind. Made new directories

--- employee-details-frontend/src/App.js
+++ employee-details-frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './css/App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EmployeeList from './EmployeeList'; // Make sure the path is correct
-import UploadPage from './UploadPage'; // Make sure the path is correct
+import EmployeeList from './pages/EmployeeList'; // Make sure the path is correct
+import UploadPage from './pages/UploadPage'; // Make sure the path is correct
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<EmployeeList />} />
+                <Route path='/' element={<Dashboard/>} />
+                <Route path="/employeelist" element={<EmployeeList />} />
                 <Route path="/upload/:id" element={<UploadPage />} />
                 <Route path="*" element={<h2>404 - Page Not Found</h2>} />
             </Routes>
