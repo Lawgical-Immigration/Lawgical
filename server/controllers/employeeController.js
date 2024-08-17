a2502fd1e9dd64afe4755e2ad83bb7759921fee0
msoncsswe <miris11260@gmail.com>
2024-08-16 20:45:19 -0400

    add route to handle retrieving employee data

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -3,28 +3,42 @@ const Employee = require('../models/employeeModel');
 
 const crypto = require("crypto");
 
-const employeeController = {};
+const employeeController = {
 
-employeeController.addEmployee = async(req, res, next) => {
+  getEmployee: async(req, res, next) => {
+    try {
+      const employees = await Employee.find({}, 'firstName lastName DOB email country').exec();
+
+      res.locals.employeeList = employees;
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error in getEmployee middleware. ERR: ${error}`,
+        message: {err: `Error retrieving employees.`}
+      })
+    }
+  },
+
+  addEmployee: async(req, res, next) => {
+    const { firstName, lastName, DOB, email, country } = req.body;
+    const newEmployee = { firstName, lastName, DOB, email, country };
+    newEmployee.employeeId = crypto.randomBytes(16).toString('hex');
   
-  const { firstName, lastName, DOB, email, country } = req.body;
-  const newEmployee = { firstName, lastName, DOB, email, country };
-  newEmployee.employeeId = crypto.randomBytes(16).toString('hex');
-
-  try {
-    const addEmployee = await Employee.create(newEmployee);
-    const {firstName, lastName} = addEmployee;
-
-    res.locals.newEmployeeName = `${firstName} ${lastName}`;
-
-    return next();
-  } catch (error) {
-    return next({
-      log: `Error in employeeController.addEmployee. ERR: ${error}`,
-      status: 406,
-      message: {err: 'Error adding employee.'}
-    })
+    try {
+      const addEmployee = await Employee.create(newEmployee);
+      const {firstName, lastName} = addEmployee;
+  
+      res.locals.newEmployeeName = `${firstName} ${lastName}`;
+  
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error in addEmployee middleware. ERR: ${error}`,
+        status: 406,
+        message: {err: 'Error adding employee.'}
+      })
+    }
   }
-}
+};
 
 module.exports = employeeController;
