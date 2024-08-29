f659bb347dfa32c9d21bb16ae0a868c009074253
msoncsswe <miris11260@gmail.com>
2024-08-29 14:27:45 -0400

    fix file path, uninstall FE dependencies in root package.json

--- employee-details-frontend/src/App.js
+++ employee-details-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EmployeeList from './EmployeeList'; // Make sure the path is correct
+import EmployeeList from './pages/EmployeeList'; // Make sure the path is correct
 import UploadPage from './pages/UploadPage'; // Make sure the path is correct
 
 function App() {
