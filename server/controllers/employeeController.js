b0e615c31b647cbdcdee42862adfdbb0a6e17972
msoncsswe <miris11260@gmail.com>
2024-08-30 16:27:25 -0400

    refactored employeeController from mongoose to supabase

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,52 +1,72 @@
-const Employee = require('../models/employeeModel');
-;
-
+// const Employee = require('../models/employeeModel');
+const supabase = require('../../database/dbConfig');
 const crypto = require("crypto");
 
 const employeeController = {
 
-  getEmployee: async(req, res, next) => {
-    try {
-      const employees = await Employee.find({}, 'status firstName lastName DOB email country').exec();
+  getEmployeeId: async(req, res, next) => {
+    const { firstName, lastName, email } = req.body;
 
-      res.locals.employeeList = employees;
-      return next();
-    } catch (error) {
+    const { data, error } = await supabase.from('employees').select('employee_id').eq('first_name', firstName).eq('last_name', lastName).eq('email', email);
+
+    if(error) {
       return next({
-        log: `Error in getEmployee middleware. ERR: ${error}`,
-        message: {err: `Error retrieving employees.`}
+        log: `Error in employeeController.getEmployeeId middleware. ERR: ${error}`,
+        message: {err: 'Error fetching employee ID. See server log for details.'}
       })
+    } else if(data.length === 0) {
+      return next({
+        log: `Employee not found in the database.`,
+        status: 404,
+        message: {err: 'Employee not found. Please try again.'}
+      })
+    } else {
+      const employeeId = data[0].employee_id;
+      res.locals = employeeId;
+      return next();
+    }
+  },
+
+  getAllEmployees: async(req, res, next) => {
+
+    const { data, error } = await supabase.from('employees').select('_status, first_name, last_name, dob, email, country');
+
+    if(error) {
+      return next({
+        log: `Error in employeeController.getAllEmployees middleware. ERR: ${error}`,
+        message: {err: 'Error fetching employees. See server log for details.'}
+      })
+    } else {
+      res.locals.employeeList = data;
+      return next();
     }
   },
 
   addEmployee: async(req, res, next) => {
     const { firstName, lastName, DOB, email, country } = req.body;
-    const newEmployee = { firstName, lastName, DOB, email, country };
 
     let empId = null;
 
     while (empId === null) {
       const tempId = crypto.randomBytes(16).toString('hex');
-      const checkIfExists = await Employee.find({employeeId: tempId});
+      
+      const { data, error } = await supabase.from('employees').select().eq('employee_id', tempId);
 
-      if(checkIfExists.length === 0) empId = tempId;
-    }
-    
-    newEmployee.employeeId = empId;
-  
-    try {
-      const addEmployee = await Employee.create(newEmployee);
-      const {firstName, lastName} = addEmployee;
-  
-      res.locals.newEmployeeName = `${firstName} ${lastName}`;
-  
-      return next();
-    } catch (error) {
+      if(data.length === 0) empId = tempId;
+    };
+
+    const { error } = await supabase.from('employees').insert({employee_id: empId, first_name: firstName, last_name: lastName, dob: DOB, email: email, country: country});
+
+    if(error) {
       return next({
         log: `Error in addEmployee middleware. ERR: ${error}`,
         status: 406,
         message: {err: 'Error adding employee.'}
       })
+    } else {
+      res.locals.newEmployeeName = `${firstName} ${lastName}`;
+  
+      return next();
     }
   }
 };
