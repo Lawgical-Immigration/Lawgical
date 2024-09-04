04056ba221d8cdb72bd94cc06ba24883e948bcaa
msoncsswe <miris11260@gmail.com>
2024-09-04 16:38:01 -0400

    change request in employeeRouter from _ to req

--- server/routers/employeeRouter.js
+++ server/routers/employeeRouter.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const { getAllEmployees, addEmployee} = require('../controllers/employeeController');
 
-router.get('/', getAllEmployees, (_, res) => {
+router.get('/', getAllEmployees, (req, res) => {
   return res.status(200).json(res.locals.employeeList);
 })
 
-router.post('/', addEmployee, (_, res) => {
+router.post('/', addEmployee, (req, res) => {
   return res.status(201).json(`${res.locals.newEmployeeName} successfully added to the database.`)
 })
 
